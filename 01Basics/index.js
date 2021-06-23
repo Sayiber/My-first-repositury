596f950be1017b5be04ac335fecf92f9f58ca66d
Kevin Bach Gravesen <sayiber@gmail.com>
2021-06-23 20:48:44 +0200

    My first commit

---
+++ 01Basics/index.js
@@ -0,0 +1,25 @@
+// This is my first JavaScript code
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+
+// Dot Notation
+person.name = 'John';
+console.log(person.name);
+
+// Bracket Notation
+person['name'] = 'Mary'
+console.log(person['name']);
+
+
+let selection = 'name';
+console.log(person[selection]);
+
+
+function great(name) {
+    console.log('Hello ' + name);
+}
+
+great('John');
+great('Mary');
